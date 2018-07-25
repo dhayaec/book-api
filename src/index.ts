411af5a65949b7360d7e748397e152c881a16715
Dhayanandhan R <dhaya.r@partner.samsung.com>
2018-07-25 11:06:46 +0530

    add test email route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import { GraphQLServer } from 'graphql-yoga';
 import { genSchema } from './utils/schema-utils';
+import { renderEmail } from './emails/emails';
 
 const server = new GraphQLServer({
   schema: genSchema()
+});
+
+server.express.get('/test-email', (req, res) => {
+  console.log(req.body);
+  const random = Math.random().toString();
+  const email = renderEmail({
+    subject: random,
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. ' +
+      'Ipsum cum distinctio temporibus error, nulla molestiae id est ' +
+      'laudantium tempore eos ut sunt sed magnam incidunt porro necessitatibus' +
+      'beatae! Eius, magni!',
+    salutation: 'Hello dude'
+  });
+  res.send(email);
 });
 
 server.start(() => console.log('localhost:4000'));
