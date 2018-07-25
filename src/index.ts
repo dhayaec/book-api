ff19ccba00409b21b22003222c11ac2075baa374
Dhayanandhan R <dhaya.r@partner.samsung.com>
2018-07-25 14:46:41 +0530

    add typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { GraphQLServer } from 'graphql-yoga';
 import { genSchema } from './utils/schema-utils';
-import { renderEmail } from './emails/emails';
+import { renderEmail } from './utils/emails/emails';
+import { createConnection } from 'typeorm';
+import { Photo } from './entity/Photo';
 
 const server = new GraphQLServer({
   schema: genSchema()
@@ -20,4 +22,52 @@ server.express.get('/test-email', (_, res) => {
   res.send(email);
 });
 
-server.start(() => console.log('localhost:4000'));
+const startServer = async () => {
+  const server = new GraphQLServer({
+    schema: genSchema(),
+    context: ({ request }) => ({
+      url: request.protocol + '://' + request.get('host'),
+      session: request.session,
+      req: request
+    })
+  });
+
+  try {
+    const connection = await createConnection({
+      name: 'default',
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3307,
+      username: 'test',
+      password: 'test',
+      database: 'test',
+      entities: [Photo],
+      synchronize: true
+    });
+    const photo = new Photo();
+    photo.name = 'Me and Bears';
+    photo.description = 'I am near polar bears';
+    photo.filename = 'photo-with-bears.jpg';
+    photo.views = 1;
+    photo.isPublished = true;
+
+    const photoRepository = connection.getRepository(Photo);
+
+    await photoRepository.save(photo);
+    console.log('Photo has been saved');
+
+    const savedPhotos = await photoRepository.find();
+    console.log('All photos from the db: ', savedPhotos);
+
+    const meAndBearsPhoto = await photoRepository.findOne({
+      name: 'Me and Bears'
+    });
+    console.log('Me and Bears photo from the db: ', meAndBearsPhoto);
+  } catch (error) {
+    console.log(error);
+  }
+
+  server.start(() => console.log('localhost:4000'));
+};
+
+startServer();
