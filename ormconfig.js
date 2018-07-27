909e4c07aff9ad4f834d79874c9fe7547e77b6e2
Dhayanandhan R <dhaya.r@partner.samsung.com>
2018-07-27 14:39:35 +0530

    seperate test config

--- ormconfig.js
+++ ormconfig.js
@@ -1,21 +1,51 @@
-const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME, DB_TYPE } = process.env;
+const {
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASS,
+  DB_NAME,
+  DB_TYPE,
+  DB_NAME_TEST
+} = process.env;
 
-module.exports = {
-  name: 'default',
-  type: DB_TYPE,
-  host: DB_HOST,
-  port: DB_PORT,
-  username: DB_USER,
-  password: DB_PASS,
-  database: DB_NAME,
-  synchronize: false,
-  logging: true,
-  entities: ['src/entity/**/*.ts'],
-  migrations: ['src/migration/**/*.ts'],
-  subscribers: ['src/subscriber/**/*.ts'],
-  cli: {
-    entitiesDir: 'src/entity',
-    migrationsDir: 'src/migration',
-    subscribersDir: 'src/subscriber'
+module.exports = [
+  {
+    name: 'development',
+    type: DB_TYPE,
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME,
+    synchronize: false,
+    logging: true,
+    entities: ['src/entity/**/*.ts'],
+    migrations: ['src/migration/**/*.ts'],
+    subscribers: ['src/subscriber/**/*.ts'],
+    cli: {
+      entitiesDir: 'src/entity',
+      migrationsDir: 'src/migration',
+      subscribersDir: 'src/subscriber'
+    }
+  },
+  {
+    name: 'test',
+    type: DB_TYPE,
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME_TEST,
+    synchronize: true,
+    logging: true,
+    dropSchema: true,
+    entities: ['src/entity/**/*.ts'],
+    migrations: ['src/migration/**/*.ts'],
+    subscribers: ['src/subscriber/**/*.ts'],
+    cli: {
+      entitiesDir: 'src/entity',
+      migrationsDir: 'src/migration',
+      subscribersDir: 'src/subscriber'
+    }
   }
-};
+];
--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,10 +1,9 @@
 import { createConnection, getConnectionOptions } from 'typeorm';
 
 export const db = async () => {
-  const connectionOptions = await getConnectionOptions();
+  const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
   return createConnection({
     ...connectionOptions,
-    name: 'default',
-    synchronize: false
+    name: 'default'
   });
 };
