bb193579773219738d5247d5a35c86f39f36a549
Dhayanandhan R <dhaya.r@partner.samsung.com>
2018-07-30 16:33:10 +0530

    add test setup

--- src/setup.ts
+++ src/setup.ts
@@ -1,13 +1,9 @@
 import { AddressInfo, Server } from 'net';
 import { startServer } from './server';
+
 let app: Server;
 export const setup = async () => {
-  beforeAll(async () => {
-    app = await startServer();
-    const { port } = app.address() as AddressInfo;
-    process.env.TEST_HOST = `http://127.0.0.1:${port}`;
-  });
-  afterAll(async () => {
-    app.close();
-  });
+  app = await startServer();
+  const { port } = app.address() as AddressInfo;
+  process.env.TEST_HOST = `http://127.0.0.1:${port}`;
 };
